e321471b08513fd5e70a137fa0eff3539730098e
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-16 17:29:35 -0800

    handleSubmit func on listItemForm

--- src/ListItemForm.js
+++ src/ListItemForm.js
@@ -3,15 +3,19 @@ import { createListItem } from './services/fetch-utils';
 
 export default function ListItemForm({ fetchItems }) {
   // you'll need to track the name and quantity in state
+  const [quantity, setQuantity] = useState(1);
+  const [name, setName] = useState('');
 
   async function handleSubmit(e) {
     e.preventDefault();
     
     // make a new list item in supabase using the form values stored in state
-
+    await createListItem(name, quantity);
     // refetch the items using the handler functionpassed down as a prop
-
+    fetchItems();
     // clear the name and quantity in state to refresh the form
+    setName('');
+    setQuantity('');
   }
 
   return (
